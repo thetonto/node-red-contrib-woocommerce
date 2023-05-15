94a1ebd59c5e80e6804acc18e12c04446d172775
ddemarco2015@gmail.com <2941791+thetonto@users.noreply.github.com>
2023-05-15 16:16:31 +1000

    Update Help

--- woocommerce.js
+++ woocommerce.js
@@ -1,8 +1,5 @@
 /* cSpell:disable */
 
-const _ = require('lodash')
-const fetch = require('node-fetch')
-
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 module.exports = function (RED) {
@@ -19,19 +16,14 @@ module.exports = function (RED) {
       consumerSecret: node.api.consumerSecret,
       version: "wc/v3"
     });
-    console.log('Woo API has been initialised ' + node.api.siteURL)
-    this.log("Something happened");
+
+    this.log('Woo API has been initialised ' + node.api.siteURL);
 
     node.on('input', function (msg, send, done) {
-      // If this is pre-1.0, 'send' will be undefined, so fallback to node.send
-      send = send || function () { node.send.apply(node, arguments) }
-
       this.status({ fill: 'green', shape: 'ring', text: 'online to woo' })
       node = this
       // parse message
       const cmdData = {}
-      // #TODO - Map against possible values and validate
-
       // Pickup any options in the incoming message.
       if (typeof msg.endpoint !== 'undefined') {
         cmdData.endpoint = msg.endpoint
@@ -45,42 +37,32 @@ module.exports = function (RED) {
           done()
         } else {
           // Fallback to node.error (pre-1.0)
-          node.error(err, msg)
+          node.error(msg)
         }
         node.status({ fill: 'red', shape: 'dot', text: 'error' })
         send(msg)
       }
-
-
-      if (typeof msg.options !== 'undefined') {
-        cmdData.modeOptions = msg.options
+      if (typeof msg.params !== 'undefined') {
+        cmdData.params = msg.params
       };
-      console.log('Payload = ' + msg.payload)
-      console.log('Compiled Command is:' + JSON.stringify(cmdData))
+     
       if (config.method == 'GET'){
-        api.get(msg.payload, cmdData.options)
+        this.log('Configured Method is ' + config.method);
+        api.get(cmdData.endpoint, cmdData.params)
           .then((response) => {
             // Successful request
             msg.payload = response
                 node.status({ fill: 'green', shape: 'dot', text: 'Connected' })
                 send(msg)
-                // Check done exists (1.0+)
-                if (done) {
-                  done()
-                }
+                done()
+
           })
           .catch((error) => {
             // Invalid request, for 4xx and 5xx statuses
             // grab the error messasge and send as payload.
-              msg.payload = err.message
+              msg.payload = error.message
               // Report back the error
-              if (done) {
-                // Use done if defined (1.0+)
-                done(err)
-              } else {
-                // Fallback to node.error (pre-1.0)
-                node.error(err, msg)
-              }
+              done(error)
               node.status({ fill: 'red', shape: 'dot', text: 'error' })
               send(msg)
           })
@@ -88,6 +70,50 @@ module.exports = function (RED) {
             // Always executed.
           });
         }; 
+      if (config.method == 'POST'){
+        api.post(cmdData.endpoint, msg.payload, cmdData.options)
+          .then((response) => {
+            // Successful request
+            msg.payload = response
+                node.status({ fill: 'green', shape: 'dot', text: 'Connected' })
+                send(msg)
+                done()  
+          })
+          .catch((error) => {
+            // Invalid request, for 4xx and 5xx statuses
+            // grab the error messasge and send as payload.
+              msg.payload = error.message
+              done(error)
+              node.status({ fill: 'red', shape: 'dot', text: 'error' })
+              send(msg)
+          })
+          .finally(() => {
+            // Always executed.
+          });
+        };
+      if (config.method == 'PUT'){
+        api.put(cmdData.endpoint, msg.payload, cmdData.options)
+          .then((response) => {
+            // Successful request
+            msg.payload = response
+                node.status({ fill: 'green', shape: 'dot', text: 'Connected' })
+                send(msg)
+                done()
+                
+          })
+          .catch((error) => {
+            // Invalid request, for 4xx and 5xx statuses
+            // grab the error messasge and send as payload.
+              msg.payload = error.message
+              // Report back the error
+              done(error)
+              node.status({ fill: 'red', shape: 'dot', text: 'error' })
+              send(msg)
+          })
+          .finally(() => {
+            // Always executed.
+          });
+        };
     })
   }
 
